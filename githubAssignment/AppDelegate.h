b4cb46206ab1ab5759b9a0f9b77c259663b01f9c
USI Training <unbounded@Mac-151.local>
2013-10-24 09:34:38 -0400

    Initial Commit

---
+++ githubAssignment/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  githubAssignment
+//
+//  Created by USI Training on 10/24/13.
+//  Copyright (c) 2013 unlimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ githubAssignment/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  githubAssignment
+//
+//  Created by USI Training on 10/24/13.
+//  Copyright (c) 2013 unlimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
