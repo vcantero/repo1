f38f4583a7b29052823c70608388c89caf8b5f4a
USI Training <unbounded@Mac-151.local>
2013-10-24 09:36:12 -0400

    test1

--- githubAssignment/AppDelegate.m
+++ githubAssignment/AppDelegate.m
@@ -12,6 +12,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    //them changes
     self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
     // Override point for customization after application launch.
     self.window.backgroundColor = [UIColor whiteColor];
