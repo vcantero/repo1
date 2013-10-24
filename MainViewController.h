e1515e7a0382a8200040ffd38cf2d48ad803e953
Aggrey Jacobs <awjacobs@Aggreys-MacBook-Air-2.local>
2013-10-24 10:12:23 -0400

    Adding Delegate

--- MainViewController.h
+++ MainViewController.h
@@ -8,7 +8,7 @@
 
 #import <UIKit/UIKit.h>
 
-@interface MainViewController : UIViewController
+@interface MainViewController : UIViewController<UITableViewDelegate>
 
 @property (strong, nonatomic) NSArray *newArray;
 
